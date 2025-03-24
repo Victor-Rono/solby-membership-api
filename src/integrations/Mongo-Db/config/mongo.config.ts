b13af42e94e1e8bcc4a76e27f84409d563efe641
Victor-Rono-Elgia <46953806+Victor-Rono@users.noreply.github.com>
2025-03-24 12:55:23 +0300

    Update mongo.config.ts

--- src/integrations/Mongo-Db/config/mongo.config.ts
+++ src/integrations/Mongo-Db/config/mongo.config.ts
@@ -2,19 +2,19 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-export const MongoConfig = {
-    user: process.env.MONGO_USER, // Replace with your desired username
-    pwd: process.env.MONGO_PWD, // Replace with a strong password
-    roles: [{ role: "root", db: process.env.MONGO_DATABASE }], // Full
-    dbName: process.env.MONGO_DATABASE,
-}
-
 // export const MongoConfig = {
-//     user: "alotofm!lkforEveryton3", // Replace with your desired username
-//     pwd: "Saedadsdada3cusdlmkmdkladkwaidjlre!P!sasdd4444tyrrawwLLL<KKK>asdasw0rd2025", // Replace with a strong password
-//     roles: [{ role: "root", db: "admin" }], // Full
-//     dbName: 'admin',
+//     user: process.env.MONGO_USER, // Replace with your desired username
+//     pwd: process.env.MONGO_PWD, // Replace with a strong password
+//     roles: [{ role: "root", db: process.env.MONGO_DATABASE }], // Full
+//     dbName: process.env.MONGO_DATABASE,
 // }
+
+export const MongoConfig = {
+    user: "doadmin!lkforEveryton3", // Replace with your desired username
+    pwd: "1E04x259z6GdV7cY", // Replace with a strong password
+    roles: [{ role: "root", db: "membership" }], // Full
+    dbName: 'admin',
+}
 
 
 
